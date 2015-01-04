9591d53c354f14b79f661b19d5d6f5f946c0e922
Tgc <tgcrap4@gmail.com>
2015-01-04 16:19:46 +0100

    Add aditional ! to msg

--- src/test/java/tgc/hello/HelloTest.java
+++ src/test/java/tgc/hello/HelloTest.java
@@ -9,7 +9,7 @@ public class HelloTest {
 	@Test
 	public void testHello() {
 		final Hello hello = new Hello();
-		assertEquals("Hello World!", hello.sayHello());
+		assertEquals("Hello World!!", hello.sayHello());
 	}
 
 }
