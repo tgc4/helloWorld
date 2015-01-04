4faa10024a1c9f95e531a5debe00fde5c5f45c45
Tgc <tgcrap4@gmail.com>
2015-01-04 16:37:00 +0100

    Test refractoring

--- src/test/java/tgc/hello/HelloTest.java
+++ src/test/java/tgc/hello/HelloTest.java
@@ -2,13 +2,20 @@ package tgc.hello;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class HelloTest {
 
+	private Hello hello;
+
+	@Before
+	public void init() {
+		hello = new Hello();
+	}
+
 	@Test
 	public void testHello() {
-		final Hello hello = new Hello();
 		assertEquals("Hello World!!", hello.sayHello());
 	}
 
